2d232fc4b1f6f5b15dab8b6f8b420b3083e36b5a
ccouch1 <59575837+ccouch1@users.noreply.github.com>
2020-02-04 11:29:49 -0700

    Update to DMIT-1508-W0RKB00K

--- DDL/School Trascript..sql
+++ DDL/School Trascript..sql
@@ -1,3 +1,62 @@
 /* ***********************
-* 
+* File: SchoolTrannscript.sql
+* Author: Chris Couch
+*
+* CREATE DATABASE SchoolTranscript
 *********************** */
+USE SchoolTranscript
+GO
+/*== Drop State,emts ==*/
+IF EXISTS (SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = 'StudentCourses')
+    DROP TABLE StudentCourses
+IF EXISTS (SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = 'Courses')
+    DROP TABLE Courses
+IF EXISTS (SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = 'Students')
+    DROP TABLE Students
+/*== Create Tables ==*/
+CREATE TABLE Students
+(
+    StudentID       int        
+        CONSTRAINT PK_Student_StudentID
+            PRIMARY KEY         NOT NULL,
+    GiveName        varchar(50) NOT NULL,
+    Surname         varchar(50) NOT NULL,
+    DateOfBirth     datetime    NOT NULL,
+    Enrolled        bit         NOT NULL,
+        CONSTRAINT DF_Students_Enrolled
+            DEFAULT (1)         NOT NULL,
+)
+
+CREATE TABLE Courses
+(  
+    Number      varchar(10)      
+        CONSTRAINT PK_Courses_Number
+            PRIMARY KEY          NOT NULL,
+    [Name]      varchar(50)      NOT NULL,
+    Credits     decimal(3, 1)    NOT NULL,
+    [Hours]     tinyint          NOT NULL,
+    Active      bit              NOT NULL,
+        CONSTRAINT DF_Courses_Active
+            DEFAULT (1)          NOT NULL,
+    Cost        money            NOT NULL,
+)
+
+CREATE TABLE StudentCourses
+(
+    StudentID       int           
+        CONSTRAINT FK_StudentCourses_StudentID_Students_StudentID
+            FOREIGN KEY REFERENCES Students(StudentID)
+                                   NOT NULL,
+    CourseNumber    varchar(10)  
+        CONSTRAINT FK_StudentCourses_CourseNumber_Courses_Number
+            FOREIGN KEY REFERENCES Courses(Number)
+                                   NOT NULL,
+    [Year]          tinyint        NOT NULL,
+    Term            char(3)        NOT NULL,
+    FinalMark       tinyint            NULL,
+    [Status]        char(1)        NOT NULL,
+    --Table-level contraint for composite keys
+    CONSTRAINT PK_StudentCourses_StudentID_CourseNumber
+        PRIMARY KEY (StudentID, CourseNumber)
+)
+/* === Drop Statements ==*/
